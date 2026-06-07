0d82315d375096ee42ac28d3203d14d0b44f404b
SreeLakshmi <sreelakshmiankireddypalli@gmail.com>
2026-06-07 16:43:20 +0530

    Time: 68 ms (51.16%), Space: 79.7 MB (90.7%) - LeetHub

---
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.java
@@ -0,0 +1,28 @@
+
+class Solution {
+    public TreeNode createBinaryTree(int[][] descriptions) {
+        Map<Integer, TreeNode> nodes = new HashMap<>();
+        Set<Integer> children = new HashSet<>();
+        for (var d : descriptions) {
+            int parent = d[0], child = d[1], isLeft = d[2];
+            if (!nodes.containsKey(parent)) {
+                nodes.put(parent, new TreeNode(parent));
+            }
+            if (!nodes.containsKey(child)) {
+                nodes.put(child, new TreeNode(child));
+            }
+            if (isLeft == 1) {
+                nodes.get(parent).left = nodes.get(child);
+            } else {
+                nodes.get(parent).right = nodes.get(child);
+            }
+            children.add(child);
+        }
+        for (var e : nodes.entrySet()) {
+            if (!children.contains(e.getKey())) {
+                return e.getValue();
+            }
+        }
+        return null;
+    }
+}
